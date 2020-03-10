226f6140804402f4a99749f120a956239f8123c9
Federico Rodriguez <federodriguez.88@gmail.com>
2020-03-09 21:41:32 -0300

    Check for typescript presence and installing if neccessary when building

--- build.sh
+++ build.sh
@@ -5,7 +5,17 @@ set -o pipefail
 
 echo Starting build...
 
-npm install -g typescript
+echo Checking build dependencies...
+# TODO(Fede): check for typescript version and give an error
+# if it is incorrect.
+if command -v tsc >/dev/null 2>&1; then
+  echo Typescript is already installed.
+else
+  echo Did not find Typescript
+  echo Installing...
+  npm install -g typescript
+fi
+
 # Reset the build directory
 rm -rf build
 mkdir -p build
@@ -16,8 +26,7 @@ cp src/svg-draw.html build/index.html
 echo .html Files copied successfully
 
 # Get js file
-# TODO(Fede): check for typescript version and give an error
-# if it is incorrect.
+
 echo Compiling .ts files...
 tsc src/svg-draw.ts --outFile build/svg-draw.js
 echo .ts Files compiled successfully
